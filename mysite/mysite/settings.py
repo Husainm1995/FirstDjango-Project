e86873f5f0e9d1a14c61bc2e7d7045d1d5123077
husainm <husainm@trinesis.com>
2019-12-19 19:58:43 +0530

    removed whitenoise compatibility

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -128,8 +128,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles').replace('\\', '/')
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 # STATICFILES_DIRS = [
 #      os.path.join(BASE_DIR, "../static").replace('\\', '/'),
 # ]
